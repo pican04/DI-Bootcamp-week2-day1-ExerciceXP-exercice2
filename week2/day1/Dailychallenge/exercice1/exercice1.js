dfd5bc64d79f2d46c2a764e9231f692051e27534
Nadia <nadiagueu@gmail.com>
2022-12-28 01:02:28 +0000

    week2 dailychallenge1

---
+++ week2/day1/Dailychallenge/exercice1/exercice1.js
@@ -0,0 +1,7 @@
+const fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+fruits.splice(0, 1); //supprimer Banana
+fruits.sort(); // mettre par ordre alphabetique
+fruits.push("Kiwi"); // ajouter kiwi a la fin
+fruits.shift(); // supprimer Apples
+fruits.reverse(); // inverser le tableau
+console.log(fruits)
